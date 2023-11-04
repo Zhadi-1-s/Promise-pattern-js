acaeb68e494790fb160b81e552c0466e15265266
Zhadi <zhadi000s@gmail.com>
2023-11-04 16:12:40 +0600

    .

--- toDo-promise pt/code.js
+++ toDo-promise pt/code.js
@@ -4,15 +4,15 @@ const main = document.getElementById("main")
 
 const get = fetch(apiUrl,{method:'GET'})
 
-let data = []
-get.then(
-    result => {
-        return result.json()
-    }
-).then(
-    users => {
-        const names = users.map(person => person.name).join("\n");
-        data = names
-        main.innerHTML = names
-    }
-)
+get.then(respone => respone.json())
+.then(users => {
+    const ul = document.getElementById("main");
+    users.foreach(user => {
+        const listItem = document.createElement("li");
+        listItem.textContent = `ID: ${user.id}, Name: ${user.name}`;
+        ul.appendChild(listItem);
+    });
+})
+.catch(error => {
+    console.error("Error fetching data:", error)
+})
